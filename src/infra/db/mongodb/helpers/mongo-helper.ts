ee7f909609f0dbd0c4c960eae64a6413b201bb99
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2021-01-27 20:35:12 -0300

    feat: add implementation and tests for AccountMongoRepository

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,26 @@
+import { Collection, MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as MongoClient,
+
+  async connect (uri: string) {
+    this.client = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+  },
+
+  async disconnect () {
+    await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  },
+
+  map (collection: any): any {
+    const { _id, ...collectionWithoutId } = collection
+    return Object.assign({}, collectionWithoutId, { id: _id })
+  }
+
+}
