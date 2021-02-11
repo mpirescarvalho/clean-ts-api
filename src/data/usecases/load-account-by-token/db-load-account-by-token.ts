09015580151bc2d267a00b63e74fc2c8af8275f3
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2021-02-10 21:26:11 -0300

    feat: add implementation and tests for DbLoadAccountByToken

---
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -0,0 +1,20 @@
+import { AccountModel } from '../../../domain/models/account'
+import { LoadAccountByToken } from '../../../domain/usecases/load-account-by-token'
+import { Decrypter } from '../../protocols/cryptography/decrypter'
+import { LoadAccountByTokenRepository } from '../../protocols/db/account/load-account-by-token-repository'
+
+export class DbLoadAccountByToken implements LoadAccountByToken {
+  constructor (
+    private readonly decrypter: Decrypter,
+    private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository
+  ) {}
+
+  async load (accessToken: string, role?: string): Promise<AccountModel> {
+    const token = await this.decrypter.decrypt(accessToken)
+    if (token) {
+      const account = await this.loadAccountByTokenRepository.loadByToken(accessToken, role)
+      return account
+    }
+    return null
+  }
+}
