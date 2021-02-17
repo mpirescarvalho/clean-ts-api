82985505020b898255f957269753d027e32252c8
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2021-02-16 21:50:57 -0300

    feat: add route /surveys/:surveyId/results

---
+++ src/main/routes/survey-result-routes.test.ts
@@ -0,0 +1,78 @@
+import app from '@/main/config/app'
+import env from '@/main/config/env'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import request from 'supertest'
+import jwt from 'jsonwebtoken'
+
+let surveyCollection: Collection
+let accountCollection: Collection
+
+const makeAccessToken = async (): Promise<string> => {
+  const res = await accountCollection.insertOne({
+    name: 'José',
+    email: 'jose.123@gmail.com',
+    password: '123'
+  })
+  const id = res.ops[0]._id
+  const accessToken = jwt.sign({ id }, env.jwtSecret)
+  await accountCollection.updateOne({
+    _id: id
+  }, {
+    $set: {
+      accessToken
+    }
+  })
+  return accessToken
+}
+
+const makeFakeSurvey = (): any => ({
+  question: 'Question',
+  answers: [{
+    answer: 'Answer 1',
+    image: 'http://image-name.com'
+  }, {
+    answer: 'Answer 2'
+  }],
+  date: new Date()
+})
+
+describe('Survey Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  describe('PUT /surveys/:surveyId/result', () => {
+    test('Should return 403 on save survey result without accessToken', async () => {
+      await request(app)
+        .put('/api/surveys/any_id/results')
+        .send({
+          answer: 'any_answer'
+        })
+        .expect(403)
+    })
+
+    test('Should return 200 on save survey result with accessToken', async () => {
+      const accessToken = await makeAccessToken()
+      const res = await surveyCollection.insertOne(makeFakeSurvey())
+      await request(app)
+        .put(`/api/surveys/${res.ops[0]._id}/results`)
+        .set('x-access-token', accessToken)
+        .send({
+          answer: 'Answer 1'
+        })
+        .expect(200)
+    })
+  })
+})
