030183b017716d5963ff9bbd0f5684e2ae8fe6c5
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2021-02-18 19:34:25 -0300

    docs: add login route docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,31 @@
+import { loginPath } from './paths/login-path'
+import { badRequest, serverError, unauthorized } from './components'
+import { accountSchema, loginParamsSchema, errorSchema } from './schemas'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
     description: 'API de enquetes em Node.js com SOLID e TDD',
     version: '1.0.0'
+  },
+  servers: [{
+    url: '/api'
+  }],
+  tags: [{
+    name: 'Login'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema,
+    error: errorSchema
+  },
+  components: {
+    badRequest,
+    serverError,
+    unauthorized
   }
 }
