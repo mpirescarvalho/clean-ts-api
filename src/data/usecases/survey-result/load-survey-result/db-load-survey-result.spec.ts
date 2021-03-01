f1cd36d113b18ecbe2cfae7002d2ad30148711de
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2021-02-28 21:04:38 -0300

    feat: add implementation and tests for LoadSurveyResult

---
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -0,0 +1,40 @@
+import { LoadSurveyResultRepository } from './db-load-survey-result-protocols'
+import { DbLoadSurveyResult } from './db-load-survey-result'
+import { mockLoadSurveyResultRepository } from '@/data/__test__'
+import { mockSurveyResultModel, throwError } from '@/domain/__test__'
+
+type SutTypes = {
+  sut: DbLoadSurveyResult
+  loadSurveyResultRepositoryStub: LoadSurveyResultRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyResultRepositoryStub = mockLoadSurveyResultRepository()
+  const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub)
+  return {
+    sut,
+    loadSurveyResultRepositoryStub
+  }
+}
+
+describe('DbLoadSurveyResult Usecase', () => {
+  test('Should call loadSurveyResultRepository', async () => {
+    const { sut, loadSurveyResultRepositoryStub } = makeSut()
+    const loadBySurveyIdSpy = jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId')
+    await sut.load('any_survey_id')
+    expect(loadBySurveyIdSpy).toHaveBeenCalledWith('any_survey_id')
+  })
+
+  test('Should throw if loadSurveyResultRepository throws', async () => {
+    const { sut, loadSurveyResultRepositoryStub } = makeSut()
+    jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId').mockImplementationOnce(throwError)
+    const promise = sut.load('any_survey_id')
+    await expect(promise).rejects.toThrow()
+  })
+
+  test('Should return surveyResultModel on success', async () => {
+    const { sut } = makeSut()
+    const surveyResultModel = await sut.load('any_survey_id')
+    expect(surveyResultModel).toEqual(mockSurveyResultModel())
+  })
+})
---
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
@@ -0,0 +1,10 @@
+import { LoadSurveyResultRepository, LoadSurveyResult, SurveyResultModel } from './db-load-survey-result-protocols'
+
+export class DbLoadSurveyResult implements LoadSurveyResult {
+  constructor (private readonly loadSurveyResultRepository: LoadSurveyResultRepository) {}
+
+  async load (surveyId: string): Promise<SurveyResultModel> {
+    const surveyResult = await this.loadSurveyResultRepository.loadBySurveyId(surveyId)
+    return surveyResult
+  }
+}
